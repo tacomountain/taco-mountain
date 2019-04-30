66384b5ff521ae251e2d0b2f20ac726ae49fbc5a
UncleJerry <jared.myhrberg@gmail.com>
2019-04-30 10:24:16 -0700

    completed ensure auth and ensure role middleware

--- test/middleware/ensure-auth.test.js
+++ test/middleware/ensure-auth.test.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const { tokenize } = require('../../lib/utils/token');
-const { ensureAuth } = require('../../lib/middleware/ensure-auth');
+const { ensureAuth, adminAuth, customerAuth } = require('../../lib/middleware/ensure-auth');
 
 describe('ensure auth', () => {
+
   it('validates a good token', done => {
     const token = tokenize({
       username: 'me'
@@ -18,4 +19,38 @@ describe('ensure auth', () => {
     };
     ensureAuth(req, res, next);
   });
+
+  it('checks for admin auth', () => {
+    const req = { user: { role: 'admin' } };
+    const res = {};
+    const next = jest.fn();
+    adminAuth(req, res, next);
+    expect(next).toHaveBeenCalledWith();
+  });
+
+  it('checks for not admin auth', () => {
+    const req = { user: { role: 'customer' } };
+    const res = {};
+    const next = jest.fn();
+    adminAuth(req, res, next);
+    expect(next).toHaveBeenCalledWith(expect.any(Error));
+  });
+
+  it('checks for customer auth', () => {
+    const req = { user: { role: 'customer' } };
+    const res = {};
+    const next = jest.fn();
+    customerAuth(req, res, next);
+    expect(next).toHaveBeenCalledWith();
+  });
+
+  it('checks for not customer auth', () => {
+    const req = { user: { role: 'waiter' } };
+    const res = {};
+    const next = jest.fn();
+    customerAuth(req, res, next);
+    expect(next).toHaveBeenCalledWith(expect.any(Error));
+  });
+
+
 });
