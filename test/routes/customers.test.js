d336a10c9141101be63f003f36e4af8cbddf3ef1
UncleJerry <jared.myhrberg@gmail.com>
2019-05-01 13:30:41 -0700

    completed top customers by rewards aggregation

--- test/routes/customers.test.js
+++ test/routes/customers.test.js
@@ -1,2 +1,12 @@
-const Customer = require('../../lib/models/Customer');
-const { seedOrders } = require('../utils/seed-data');
+const { getAdminAgent } = require('../utils/data-helper');
+
+describe('aggregations', () => {
+  it('get top 10 customers', () => {
+    return getAdminAgent()
+      .get('/api/v1/customers/topRewards')
+      .then(res => {
+        expect(res.body).toHaveLength(10); 
+        expect(res.body[0].rewards).toBeGreaterThanOrEqual(res.body[1].rewards);
+      });
+  });
+});
