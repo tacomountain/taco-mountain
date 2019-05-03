e678f120d7980049eb6f11797c92a419b753bb7e
Anna Ridenour <annamridenour@gmail.com>
2019-05-03 09:42:31 -0700

    menu fix cont

--- client/inquirer/utils/layout-menu.js
+++ client/inquirer/utils/layout-menu.js
@@ -2,7 +2,6 @@ const agent = require('./requester');
 const inquirer = require('inquirer');
 const chalkPipe = require('chalk-pipe');
 const chance = require('chance').Chance();
-const figlet = require('figlet');
 
 const colors = ['#dd8080', '#ffac63', '#fce95d', '#c7fc5d', '#9dff7a', '#89ffae', '#9082ff', '#b266ff', '#fa84ff', '#ff5e5e'];
 
