3a3ea1646b75e0e3f6ffac289e2dc61fc829f9a2
Tommy Tran <tranttommy95@gmail.com>
2019-05-02 17:30:23 -0700

    admin sign out

--- client/inquirer/admin/admin-menu.js
+++ client/inquirer/admin/admin-menu.js
@@ -3,20 +3,19 @@ const inquirer = require('inquirer');
 var adminTasks = [
   {
     type: 'list',
-    name: 'adminTask',
+    name: 'task',
     message: 'What will you do?',
-    choices: ['Edit Menu', 'Analytics']
+    choices: ['Edit Menu', 'Analytics', 'Sign Out']
   }
 ];
 
-module.exports = () => inquirer.prompt(adminTasks).then(task => {
-  switch(task.adminTask) {
+module.exports = () => inquirer.prompt(adminTasks).then(({ task }) => {
+  switch(task) {
     case 'Edit Menu':
-      require('./menu/edit-menu')();
-      break;
+      return require('./menu/edit-menu')();
     case 'Analytics':
-      require('./analytics/analytics')();
-      break;
+      return require('./analytics/analytics')();
+    case 'Sign Out':
+      return console.log('You are signed out.');
   }
 });
-
